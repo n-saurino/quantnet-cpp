36a29adee519fd6dc4e94ab9b19421c1e312309d
Nigel Saurino <nigelsaurino@Nigels-MacBook-Air.local>
2023-12-23 08:20:48 -0600

    Finished section 3.4 and started section 3.5

---
+++ level_5/section_3_5/ex2/Shape.h
@@ -0,0 +1,28 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+namespace Saurino {
+	namespace CAD {
+        class Shape
+        {
+        private:
+            /* data */
+            int m_id;
+
+        public:
+            Shape(/* args */);
+            Shape(int);
+            Shape(const Shape &source);
+            ~Shape();
+            // Member operator overloading
+            Shape& operator = (const Shape& source);
+            int ID() const;
+            virtual std::string ToString() const;
+        };
+    }
+}
+
+#endif
