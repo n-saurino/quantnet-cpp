9368396c5fce884fbc99b38482b79b42372add68
Nigel Saurino <nigelsaurino@gmail.com>
2024-01-31 14:58:00 -0500

    Setting up first exercise in option pricing assignment

---
+++ level_9/exA/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int, char**){
+
+    /*
+    T (expiry time/maturity): T=1 means one year
+    K (strike price)
+    sig (volatility)
+    r (risk-free interest rate)
+    S (current stock price where we wish to price the option)
+    C (call option price)
+    P (puth option price)
+    */
+
+    std::cout << "Hello, from EXA!\n";
+}
